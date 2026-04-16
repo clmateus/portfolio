a7846a90e13973234c50854f37fff17a71646a5b
clmateus <cl.mateuscosta@gmail.com>
2026-04-16 15:10:23 -0300

    update

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField(blank=True, null=True)
+    description = models.TextField()
+    techstack = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.title}'
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from .data import hard_skills, projects
+# from .data import hard_skills, projects
+from .models import Skill, Project
 
 def home(request):
-    return render(request, 'home.html', {'hard_skills': hard_skills})
+    skills = Skill.objects.all()
+    return render(request, 'home.html', {'skills': skills})
 
 def projects_view(request):
+    projects = Project.objects.all()
     return render(request, 'projects.html', {'projects': projects})
 
 def details(request, id):
-    project = projects.get(id)
+    project = Project.objects.get(id=id)
     return render(request, 'details.html', {'project': project})
